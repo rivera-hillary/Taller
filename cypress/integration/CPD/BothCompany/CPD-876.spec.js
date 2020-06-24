5234738f06ecdfabf2b32eb5398df60abe1befee
rivera-hillary <hrivera@wawand.co>
2020-06-24 17:46:43 -0500

    place order

---
+++ cypress/integration/CPD/BothCompany/CPD-876.spec.js
@@ -0,0 +1,19 @@
+/// <reference types="Cypress" />
+
+describe('CPD - 876', () => {
+    beforeEach(() => {
+        cy.visit('/');
+        // Otis — M3 SUPER ADMIN
+        cy.login('akore');
+        cy.wait(7000);
+        cy.usersBothCompany();
+        cy.wait(6000);
+        cy.url().should('include','users');
+    });
+    it('Send welcome emails to all users',() => {
+        cy.get('.d-flex > .btn').click();
+        cy.get('.swal2-popup').should('be.visible');
+        cy.get('#swal2-title').contains('Send Welcome Emails');
+        cy.get('#swal2-content').contains("Are you sure you'd like to send welcome emails to all users who have not yet received one?");
+    });
+});
--- cypress/integration/CPD/BothCompany/place-order-button.spec.js
+++ cypress/integration/CPD/BothCompany/place-order-button.spec.js
@@ -3,14 +3,13 @@
 describe('CPD - 834', () => {
     beforeEach(() => {
         cy.visit('/');
-        cy.login('zion');
+        cy.login('akore');
         cy.wait(1000);
-        cy.get('.d-flex > [href="/client/2807/orders/1158594/"]').click()
-        cy.url().should('include','/orders');
+        cy.usersBothCompany();
+        cy.url().should('include','/users');
     });
     it('Place order button',() => {
-        cy.get('.actions-bar > .modal-orders > #dropdownMenuButton').click();
-        cy.get('.type-order-card > .btn').click();
-        cy.get('#BuildSelectedDeviceBtn').contains('Place Order');
+        cy.get(':nth-child(1) > :nth-child(1) > .primary-lighter-color').click();
+        cy.get('.place-order-btn').should('be.visible').contains('Place Order');
     });
 });
