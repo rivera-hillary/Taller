c07cb2cdb47c10380fdb0364bd21876aa61cc358
rivera-hillary <hrivera@wawand.co>
2020-07-09 18:11:55 -0500

    Alert message

--- cypress/integration/CPD/BothCompany/CPD-895.spec.js
+++ cypress/integration/CPD/BothCompany/CPD-895.spec.js
@@ -13,9 +13,10 @@ describe('CPD - 895', () => {
         cy.get('.primary-lighter-color').click();
         cy.get('.btn-options-main-button').click();
         cy.get('.justify-content-between > .primary-color').click();
-        cy.get('[data-reference="2"] > .d-flex.mb-3 > :nth-child(2) > .checkbox-entity-device').click();
-        cy.get('[data-reference="2"] > .d-flex.mb-3 > :nth-child(2) > .checkbox-entity-phone-line').click();
-        cy.get('[data-reference="2"] > .associations-wrapper > .entity-type-phone-line > .entity-selector > .form-group > .choices > .choices__inner').click().get('[id="choices--user-UsersDevicesPhoneLines2PhoneLineID-item-choice-3"]').click();
+        cy.get('[data-reference="3"] > .d-flex.mb-3 > :nth-child(2) > .checkbox-entity-device').click();
+        cy.get('[data-reference="3"] > .d-flex.mb-3 > :nth-child(2) > .checkbox-entity-phone-line').click();
+        cy.get('[data-reference="3"] > .associations-wrapper > .entity-type-phone-line > .entity-selector > .form-group > .choices > .choices__inner > .choices__list > .choices__item').click().get('[data-reference="3"] > .associations-wrapper > .entity-type-phone-line > .entity-selector > .form-group > .choices > .choices__list--dropdown > .choices__input').type('5463864747{enter}');
+        cy.get('[data-reference="3"] > .associations-wrapper > .entity-type-device > .entity-selector > .form-group > .choices > .choices__inner > .choices__list > .choices__item').click().get('[data-reference="3"] > .associations-wrapper > .entity-type-device > .entity-selector > .form-group > .choices > .choices__list--dropdown > .choices__input').type('00000000000000001{enter}');
         cy.get('.action-container > .btn-primary').click();
         cy.get('.entity-type-phone-line > .entity-selector > .form-group > .invalid-feedback').contains('This line is repeated on the form.');
         cy.get('.entity-type-device > .entity-selector > .form-group > .invalid-feedback').contains('This device is repeated on the form.');
